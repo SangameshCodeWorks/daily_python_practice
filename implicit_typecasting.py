ef27f9a2b5b7c0022430016c53d489aa4b4f89db
SangameshCodeWorks <sanguforwork123@gmail.com>
2026-09-22 21:17:11 +0530

    22/09/2026 todays practice on type casting

---
+++ implicit_typecasting.py
@@ -0,0 +1,40 @@
+b = True
+i = 5
+f = 2.1
+c = 3+4j
+
+b_i = b+i
+print(b_i)
+print(type(b_i))
+
+print("----------------------------------------")
+
+b_f = b+f
+print(b_f)
+print(type(b_f))
+
+print("----------------------------------------")
+
+b_c = b+c
+print(b_c)
+print(type(b_c))
+
+print("----------------------------------------")
+
+i_f = i+f
+print(i_f)
+print(type(i_f))
+
+print("----------------------------------------")
+i_c = i+c
+print(i_c)
+print(type(i_c))
+
+print("----------------------------------------")
+
+f_c = f+c
+print(f_c)
+print(type(f+c))
+
+print("----------------------------------------")
+
---
+++ typecasting3.py
@@ -0,0 +1,15 @@
+l = [10,20,30]
+print(tuple(l))
+print(set(l))
+print(str(l))
+#print(dict(l))
+
+print("----------------------------------------")
+
+t = (10,20,30)
+print(set(t))
+print(list(t))
+print(str(t))
+#print(dict(t))
+
+
